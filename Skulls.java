1a470b968d39b659282fe43c782fdf49a00c701c
Sp3eex <46614224+Sp3eex@users.noreply.github.com>
2019-08-02 20:13:05 +0200

    Update Skulls.java

--- Skulls.java
+++ Skulls.java
@@ -6,6 +6,7 @@ import org.bukkit.Material;
 import org.bukkit.SkullType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
@@ -13,21 +14,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("skull") && sender instanceof Player) {
-			Player player = (Player) sender;
-			ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (byte) SkullType.PLAYER.ordinal());
-	        SkullMeta skullMeta = (SkullMeta) Bukkit.getItemFactory().getItemMeta(Material.SKULL_ITEM);
-	        skullMeta.setOwner(args[0]);
-	        skull.setItemMeta(skullMeta);
-	        if (args[0] != null) {
-	        	skullMeta.setDisplayName(ChatColor.RESET + skullMeta.getOwningPlayer().toString());
-	        }
-	        player.getInventory().addItem(skull); return true;
-		} else if (cmd.getName().equalsIgnoreCase("skull") && args.length == 0) {
-			sender.sendMessage(ChatColor.RED + "/skull <playername>"); 
-			return true;
-		}
-		return true;
-	}	
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof ConsoleCommandSender) { return true; }
+	    if (cmd.getName().equalsIgnoreCase("skull") && args.length == 1) {
+            Player player = (Player) sender;
+            ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (byte) SkullType.PLAYER.ordinal());
+            SkullMeta skullMeta = (SkullMeta) Bukkit.getItemFactory().getItemMeta(Material.SKULL_ITEM);
+            skullMeta.setOwner(args[0]);
+            skull.setItemMeta(skullMeta);
+            if (args[0] != null) {
+                skullMeta.setDisplayName(ChatColor.RESET + skullMeta.getOwningPlayer().toString());
+            }
+            player.getInventory().addItem(skull); 
+            return true;
+        } else if (cmd.getName().equalsIgnoreCase("skull") && args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "/skull <playername>"); 
+            return true;
+        }
+    return true;
+    }	
 }

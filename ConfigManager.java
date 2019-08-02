3ab2ae27ff40900c697108aef87983a4609f2d45
Sp3eex <46614224+Sp3eex@users.noreply.github.com>
2019-08-02 20:15:01 +0200

    Update ConfigManager.java

--- ConfigManager.java
+++ ConfigManager.java
@@ -8,42 +8,43 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ConfigManager {
+public class ConfigManager extends JavaPlugin {
 
-    public ConfigManager plugin;
-    public FileConfiguration fileconfig;
-    public File file;
+	public ConfigManager plugin;
+	public FileConfiguration fileconfig;
+	public File file;
 
-    public void SetupCustomConfig(String File) {
-        if (!Main.instance.getDataFolder().exists()) {
-            Main.instance.getDataFolder().mkdir();
-        }
-        file = new File(Main.instance.getDataFolder(), FileName);
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-                Bukkit.getServer().getConsoleSender().sendMessage("The " + FileName + " file has been created.");
-            } catch (IOException e) {
-                Bukkit.getServer().getConsoleSender().sendMessage("Could not create the " + FileName + " file.");
-            }
-        }
-        fileconfig = YamlConfiguration.loadConfiguration(file);
-    }
+	public void SetupCustomConfig(String File) {
+		if (!getDataFolder().exists()) {
+			getDataFolder().mkdir();
+		}
+		file = new File(getDataFolder(), FileName);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+				Bukkit.getServer().getConsoleSender().sendMessage("The " + FileName + " file has been created.");
+			} catch (IOException e) {
+				Bukkit.getServer().getConsoleSender().sendMessage("Could not create the " + FileName + " file.");
+			}
+		}
+		fileconfig = YamlConfiguration.loadConfiguration(file);
+	}
 
-    public FileConfiguration getFileConfig() {
-        return fileconfig;
-    }
+	public FileConfiguration getFileConfig() {
+		return fileconfig;
+	}
 
-    public void saveCustomConfig() {
-        try {
-            fileconfig.save(file);
-            Bukkit.getServer().getConsoleSender().sendMessage("The players.yml file has been saved.");
-        } catch (IOException e) {
-            Bukkit.getServer().getConsoleSender().sendMessage("Could not save the players.yml file.");
-        }
-    }
+	public void saveCustomConfig() {
+		try {
+			fileconfig.save(file);
+			Bukkit.getServer().getConsoleSender().sendMessage("The players.yml file has been saved.");
 
-    public void reloadCustomConfig() {
-        fileconfig = YamlConfiguration.loadConfiguration(file);
-    }
+		} catch (IOException e) {
+			Bukkit.getServer().getConsoleSender().sendMessage("Could not save the players.yml file.");
+		}
+	}
+
+	public void reloadCustomConfig() {
+		fileconfig = YamlConfiguration.loadConfiguration(file);
+	}
 }

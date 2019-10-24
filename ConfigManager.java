13a1bdab382da9641565dc5be8c080ac8546c01e
Sp3eex <46614224+Sp3eex@users.noreply.github.com>
2019-10-24 18:14:31 +0200

    Update ConfigManager.java

--- ConfigManager.java
+++ ConfigManager.java
@@ -1,49 +1,47 @@
-package _UtilsClasses;
+package me.Sp3eex.Crates;
 
 import java.io.File;
 import java.io.IOException;
 
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class ConfigManager extends JavaPlugin {
+public class ConfigManager {
 
-    public ConfigManager plugin;
-    public FileConfiguration fileconfig;
-    public File file;
-
-    public void SetupCustomConfig(String File) {
-        if (!getDataFolder().exists()) {
-            getDataFolder().mkdir();
+	private static FileConfiguration config;
+	private static File file;
+	
+	public static FileConfiguration GetConfigFile() {
+        return config;
+    }
+	
+	public static void SaveConfigFile(String configName) {
+    	try {
+    		File tfile = new File(Main.Instance().getDataFolder(), configName);
+			config.save(tfile); 
+			file = new File(Main.Instance().getDataFolder(), configName);
+		} catch (IOException e) { 
+			Main.Instance().getLogger().info("Could not save the " + config.getName() + " file.");
+		}
+    }
+	
+	public static void CreateConfigFile(String configName) {
+		if (!Main.Instance().getDataFolder().exists()) {
+			Main.Instance().getDataFolder().mkdir();
         }
-        file = new File(getDataFolder(), FileName);
+        file = new File(Main.Instance().getDataFolder(), configName);
         if (!file.exists()) {
             try {
                 file.createNewFile();
-                Bukkit.getServer().getConsoleSender().sendMessage("The " + FileName + " file has been created.");
+                config = YamlConfiguration.loadConfiguration(file);
+                config.save(file);
+                Main.Instance().getLogger().info("Config file created.");
             } catch (IOException e) {
-                Bukkit.getServer().getConsoleSender().sendMessage("Could not create the " + FileName + " file.");
+            	Main.Instance().getLogger().info("Could not create the " + configName + " file.");
             }
+        } else {
+        	config = YamlConfiguration.loadConfiguration(file);
+        	Main.Instance().getLogger().info("Config file loaded.");
         }
-    fileconfig = YamlConfiguration.loadConfiguration(file);
-    }
-
-    public FileConfiguration getFileConfig() {
-        return fileconfig;
-    }
-
-    public void saveCustomConfig() {
-        try {
-            fileconfig.save(file);
-            Bukkit.getServer().getConsoleSender().sendMessage("The " + FileName + " file has been saved.");
-        } catch (IOException e) {
-            Bukkit.getServer().getConsoleSender().sendMessage("Could not save the " + FileName + " file.");
-        }
-    }
-
-    public void reloadCustomConfig() {
-        fileconfig = YamlConfiguration.loadConfiguration(file);
-    }
+	}
 }
